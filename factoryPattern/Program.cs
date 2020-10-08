5334cb155f42162519d43dabdca26317b9f07d71
kilik42 <marvinevins@yahoo.com>
2020-10-07 21:02:05 -0500

    working on factory pattern implementation

--- factoryPattern/Program.cs
+++ factoryPattern/Program.cs
@@ -6,7 +6,19 @@ namespace factoryPattern
     {
         static void Main(string[] args)
         {
-            
+            int numOfWheels;
+            bool input = false;
+            do
+            {
+                Console.WriteLine("Enter the amount of tires for the  vehicle you want to create");
+
+                input = int.TryParse(Console.ReadLine(), out numOfWheels);
+
+            } while (!input);
+
+            var vehicle = VehicleFactory.GetVehicle(numOfWheels);
+            vehicle.Drive();
+
         }
     }
 }
